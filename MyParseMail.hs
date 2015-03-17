80c869b5eec78472deeb1b62137314cda31eb466
maximilianhuber <mail@maximilian-huber.de>
2015-03-17 22:41:50 +0100

    some small changes

--- MyParseMail.hs
+++ MyParseMail.hs
@@ -10,18 +10,22 @@ import           Common
 
 import           Debug.Trace
 
+--------------------------------------------------------------------------------
 parseMail :: Mail -> [String] -> Mail
 parseMail m ls =  parseMail' m ls ""
 
+--------------------------------------------------------------------------------
 parseMail' :: Mail -> [String] -> String -> Mail
 parseMail' m []     r = parseMail'' m r
-#if 1
-parseMail' m ([]:ls) r = parseMail'' m r
-#else
+#if 0
 parseMail' m ([]:ls) r = parseMail'' (m { content = unlines ls }) r
+#else
+parseMail' m ([]:ls) r = parseMail'' m r
 #endif
 parseMail' m (l:ls) r | " " `isPrefixOf` l = parseMail' m ls (r++l)
                       | otherwise          = parseMail' (parseMail'' m r) ls l
+
+--------------------------------------------------------------------------------
 parseMail'' :: Mail -> String -> Mail
 parseMail'' m r | "From:"    `isPrefixOf` r = m { from    = remKW  r }
                 | "Subject:" `isPrefixOf` r = m { subject = remKW  r }
