82c733166b86a7f3932a1c5c2cdb32921d1518ff
maximilianhuber <mail@maximilian-huber.de>
2015-04-20 23:50:53 +0200

    add help-function to the script

--- mySortMaildir.sh
+++ mySortMaildir.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-pushd "$(dirname "$0")/src" >/dev/null
-./mySortMaildir.hs
-popd >/dev/null
+if [[ -f "$(dirname "$0")/src/mySortMaildir.hs" ]]; then
+  pushd "$(dirname "$0")/src" >/dev/null
+  ./mySortMaildir.hs
+  popd >/dev/null
+else
+  cat << EOF
+The file 
+  $(dirname "$0")/src/mySortMaildir.hs
+does not exist. This means, that you have to follow the README and build your
+own set of rules.
+EOF
+  exit 1
+fi
